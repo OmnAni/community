79c0f9d4f1b2d6741b3b1286ef6daef65a66bbb3
OmnAni <zhangxu1998cn@outlook.com>
2020-02-05 21:33:47 +0800

    add githubuser's avatar

--- src/main/java/com/ani/community/mapper/UserMapper.java
+++ src/main/java/com/ani/community/mapper/UserMapper.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface UserMapper {
-    @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified,avatar_url) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
 
     @Select("SELECT * FROM user WHERE token=#{token} ")
